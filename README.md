d9daab3fe7936c7d8e440c5bb674eb511c7d27a0
reiserx <leonzone58@gmail.com>
2018-04-08 16:16:16 +0800

    complete permission demo

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # goengine
 🚀 A Android develop library
+
+this library contain core function:
+
+- Net Request
+- Permission
+- ImageLoader
+
+
+[Permission](https://github.com/leonzone/goengine/wiki)
+
--- app/src/main/java/com/gouuse/goenginesample/MainActivity.java
+++ app/src/main/java/com/gouuse/goenginesample/MainActivity.java
@@ -2,8 +2,10 @@ package com.gouuse.goenginesample;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -12,6 +14,8 @@ import com.gouuse.goengine.permission.GoPermission;
 import com.gouuse.goengine.permission.Rationale;
 import com.gouuse.goengine.permission.RequestExecutor;
 import com.gouuse.goengine.permission.SettingService;
+import com.gouuse.goenginesample.constant.DialogAction;
+import com.gouuse.goenginesample.utils.DialogUtil;
 import com.gouuse.goenginesample.view.ImageListActivity;
 import com.gouuse.goenginesample.view.UploadDownActivity;
 import com.gouuse.goenginesample.view.login.LoginActivity;
@@ -27,35 +31,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btn_1).setOnClickListener(this);
         findViewById(R.id.btn_2).setOnClickListener(this);
         findViewById(R.id.btn_3).setOnClickListener(this);
-
-
-
-
+        //test Permission
         GoPermission.with(this)
                 .permission(Manifest.permission.CALL_PHONE)
+                //if user denied the permission, you should explain why you need the permission
                 .rationale(new Rationale() {
                     @Override
                     public void showRationale(Context context, List<String> permissions, RequestExecutor executor) {
-                        executor.execute();
+                        DialogUtil.show(MainActivity.this, "why i need th permission", new DialogUtil.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull DialogInterface dialog, @NonNull DialogAction dialogAction) {
+                                if (dialogAction == DialogAction.POSITIVE) {
+                                    //permission request continue
+                                    executor.execute();
+                                } else {
+                                    //permission request cancel
+                                    executor.cancel();
+                                }
+                            }
+                        });
+
+
                     }
                 })
                 .onGranted(new Action() {
                     @Override
                     public void onAction(List<String> permissions) {
-
+                        //permission  request allowed
                     }
                 })
                 .onDenied(new Action() {
+                    //permission  request denied
                     @Override
                     public void onAction(List<String> permissions) {
-
+                        //you can use this method check the permission is always denied
                         if (GoPermission.hasAlwaysDeniedPermission(MainActivity.this, permissions)) {
-                            // 这里使用一个Dialog展示没有这些权限应用程序无法继续运行，询问用户是否去设置中授权。
-                            SettingService settingService = GoPermission.permissionSetting(MainActivity.this);
-                            // 如果用户同意去设置：
-                            settingService.execute();
-                            // 如果用户不同意去设置：
-                            settingService.cancel();
+                            // ask to go to Settings open permission
+                            DialogUtil.show(MainActivity.this, "go to setting", new DialogUtil.SingleButtonCallback() {
+                                @Override
+                                public void onClick(@NonNull DialogInterface dialog, @NonNull DialogAction dialogAction) {
+                                    SettingService settingService = GoPermission.permissionSetting(MainActivity.this);
+                                    if (dialogAction == DialogAction.POSITIVE) {
+                                        //go to Settings
+                                        settingService.execute();
+                                    } else {
+                                        //cancel
+                                        settingService.cancel();
+                                    }
+                                }
+                            });
+
+
                         }
 
                     }
